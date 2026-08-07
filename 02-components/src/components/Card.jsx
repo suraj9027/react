34339216f58e4cdd23fce3eba6cb2d76301d1b90
Suraj <surajbhandari499@gmail.com>
2026-08-07 20:02:46 +0530

    Add 02-components folder

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,13 @@
+function Card()
+{
+    return (
+        <div>
+            <div className="card">
+                    <h1>Hello Welcome</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, iure!</p>
+                  </div>
+        </div>
+    )
+}
+
+export default Card
