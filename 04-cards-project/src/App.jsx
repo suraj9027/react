8dc6ab7dd49388a8aba1eface5ba86afabbd21f7
Suraj <surajbhandari499@gmail.com>
2026-08-11 21:41:08 +0530

    Update job cards and logos

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -3,14 +3,130 @@ import Card from './components/Card'
 
  
 const App = () => {
+const jobOpenings = [
+  {
+    brandLogo: "https://www.google.com/s2/favicons?domain=google.com&sz=128",
+    name: "Google",
+    datePosted: "5 days ago",
+    post: "Frontend Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$45/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://www.google.com/s2/favicons?domain=microsoft.com&sz=128",
+    name: "Microsoft",
+    datePosted: "10 weeks ago",
+    post: "Backend Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$35/hour",
+    location: "Bangalore, India"
+  },
+  {
+    brandLogo: "https://www.google.com/s2/favicons?domain=amazon.com&sz=128",
+    name: "Amazon",
+    datePosted: "2 days ago",
+    post: "Full Stack Developer",
+    tag1: "Part Time",
+    tag2: "Senior Level",
+    pay: "$40/hour",
+    location: "Hyderabad, India"
+  },
+  {
+    brandLogo: "https://www.google.com/s2/favicons?domain=meta.com&sz=128",
+    name: "Meta",
+    datePosted: "1 week ago",
+    post: "Frontend Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$50/hour",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://www.google.com/s2/favicons?domain=apple.com&sz=128",
+    name: "Apple",
+    datePosted: "3 days ago",
+    post: "Backend Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$48/hour",
+    location: "Bangalore, India"
+  },
+  {
+    brandLogo: "https://www.google.com/s2/favicons?domain=netflix.com&sz=128",
+    name: "Netflix",
+    datePosted: "6 days ago",
+    post: "Frontend Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$38/hour",
+    location: "Delhi, India"
+  },
+  {
+    brandLogo: "https://www.google.com/s2/favicons?domain=adobe.com&sz=128",
+    name: "Adobe",
+    datePosted: "2 weeks ago",
+    post: "UI/UX Designer",
+    tag1: "Part Time",
+    tag2: "Junior Level",
+    pay: "$30/hour",
+    location: "Noida, India"
+  },
+  {
+    brandLogo: "https://www.google.com/s2/favicons?domain=ibm.com&sz=128",
+    name: "IBM",
+    datePosted: "4 days ago",
+    post: "Backend Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$42/hour",
+    location: "Pune, India"
+  },
+  {
+    brandLogo: "https://www.google.com/s2/favicons?domain=oracle.com&sz=128",
+    name: "Oracle",
+    datePosted: "1 week ago",
+    post: "Frontend Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$32/hour",
+    location: "Hyderabad, India"
+  },
+  {
+    brandLogo: "https://www.google.com/s2/favicons?domain=salesforce.com&sz=128",
+    name: "Salesforce",
+    datePosted: "3 weeks ago",
+    post: "Full Stack Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$44/hour",
+    location: "Mumbai, India"
+  }
+];
   return (
-    
-    <div className="parent">
-      <Card/>  
-      <Card/>  
-    </div>
-    
-  )
+  <div className="parent">
+    {jobOpenings.map(function(elem, idx) {
+      return (
+        <div key={idx}>
+          <Card
+            company={elem.name}
+            datePosted={elem.datePosted}
+            post={elem.post}
+            pay={elem.pay}
+            tag1={elem.tag1}
+            tag2={elem.tag2}
+            loc={elem.location}
+            brandLogo={elem.brandLogo}
+          />
+        </div>
+      )
+    })}
+  </div>
+)
 }
 
 export default App
+ 
+
--- 04-cards-project/src/components/Card.jsx
+++ 04-cards-project/src/components/Card.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
 import {Bookmark} from 'lucide-react'
 
-const Card = () => {
+const Card = (props) => {
+
   return (
      <div className="card">
         <div>
           <div className="top">
-          <img src="https://images.icon-icons.com/2699/PNG/512/amazon_logo_icon_169611.png" alt="" />
+            <img src={props.brandLogo} alt="Logo" />
           <button>Save <Bookmark size={13}/></button>
         </div>
         <div className="center">
-          <h3>Amazon <span>5 days ago</span></h3>
-          <h2>Senior UI/UX Designer</h2>
+          <h3>{props.company} <span>{props.datePosted}</span></h3>
+          <h2>{props.post}</h2>
           <div className='tag'>
-            <h4>Part Time</h4>
-            <h4>Senior Level</h4>
+            <h4>{props.tag1}</h4>
+            <h4>{props.tag2}</h4>
           </div>
         </div>
         </div>
         
         <div className="bottom">
            <div>
-              <h3>$120/hr</h3>
-              <p>Mumbai, India</p>
+              <h3>{props.pay}</h3>
+              <p>{props.loc}</p>
             </div>
             <button>Apply now</button>
            </div>
